173b9a77d8a6bb1e5279c01bf2a3fe4041080bbf
Agustin Miura <agustin.miura@gmail.com>
2020-09-26 21:16:34 -0300

    First commit

---
+++ lock-practice/src/main/java/ar/com/miura/App.java
@@ -0,0 +1,13 @@
+package ar.com.miura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
